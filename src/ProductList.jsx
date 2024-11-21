bd141f38fef26cca18e0ccc129696f8d5c03fcc2
Andrei <andreibalan258@gmail.com>
2024-11-21 05:11:06 -0500

    edited ProductList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,20 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice'; 
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
+    
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true,
+        }));
+    };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -260,6 +270,24 @@ const handlePlantsClick = (e) => {
                     </a>
                 </div>
               
+            </div>
+            <div className="product-grid">
+            {plantsArray.map((category, index) => (
+                <div key={index}>
+                    <h1><div>{category.category}</div></h1>
+                    <div className="product-list">
+                        {category.plants.map((plant, plantIndex) => (
+                        <div className="product-card" key={plantIndex}>
+                            <img className="product-image" src={plant.image} alt={plant.name} />
+                            <div className="product-title">{plant.name}</div>
+                            <div className="product-title">{plant.description}</div>
+                            <div className="product-title">{plant.cost}</div>
+                            <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                        </div>
+                        ))}
+                    </div>
+                </div>
+                ))}
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
